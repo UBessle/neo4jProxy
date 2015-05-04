0f281bf03af98f139388429f919ff594b2a43c88
Uwe Bessle <Uwe.Bessle@iteratec.de>
2015-05-04 08:12:35 +0200

    working Neo4jProxyController which copies all headers from neo4j response to proxy response

--- src/main/groovy/org/bessle/neo4j/proxy/Neo4jProxyController.groovy
+++ src/main/groovy/org/bessle/neo4j/proxy/Neo4jProxyController.groovy
@@ -1,12 +1,14 @@
 package org.bessle.neo4j.proxy
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.http.ResponseEntity
 import org.springframework.http.HttpStatus
 
 @RestController
@@ -21,12 +23,44 @@ class Neo4jProxyController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     String cypher(@RequestBody String requestCypher) {
-        println "requestCypher: ${requestCypher}"
-        def result = neo4jProxyService.getCypherResult(requestCypher)
-        println "result: ${result}"
-    	/*
-    	HttpHeaders headers = new HttpHeaders()
-    	return new ResponseEntity<String>(result, headers, HttpStatus.OK)
+            println "Neo4jProxyController.cypher()"
+            println "requestCypher: ${requestCypher}"
+        def response = neo4jProxyService.getCypherResult(requestCypher)
+        println "response.status: ${response.status}"
+        println "response.data: ${response.data}"
+        println "response.headers: "
+        response.headers.each() {
+            println "    ${it.toString()}"
+        }
+        return response.data
+        /*
+        HttpHeaders headers = new HttpHeaders()
+        return new ResponseEntity<String>(result, headers, HttpStatus.OK)
         */
     }
+
+    @RequestMapping(value = "/cypher2", method = RequestMethod.POST)
+    ResponseEntity<String> cypher2(@RequestBody String requestCypher) {
+            println "Neo4jProxyController.cypher2()"
+            println "requestCypher: ${requestCypher}"
+        def response = neo4jProxyService.getCypherResult(requestCypher)
+            println "response.status: ${response.status}"
+            println "response.data: ${response.data}"
+            println "response.headers: "
+
+        HttpHeaders headers = new HttpHeaders()
+        response.headers.each() {
+            println "    ${it.toString()}"
+            headers.add(it.name, it.value)
+        }
+
+        return new ResponseEntity<String>(response.data.toString(), headers, HttpStatus.valueOf(response.status))
+
+        /*
+        HttpHeaders headers = new HttpHeaders()
+        return new ResponseEntity<String>(result, headers, HttpStatus.OK)
+        */
+    }
+
+
 }
